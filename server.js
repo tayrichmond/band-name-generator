7388194faf3050639a218c0e1317a3360095cb3e
Taylor Richmond <taylorichmond@gmail.com>
2015-06-08 20:53:47 -0700

    add random json pull

--- server.js
+++ server.js
@@ -21,3 +21,17 @@ app.get("/fact", function (req, res) {
 
   res.send(randomFacts[index]);
 });
+
+var jokes = [
+  { setup: "What's the difference between a guitar and a fish?",
+    punchline: "You can't tuna fish." },
+  { setup: "What do you get when you cross a cow and a duck?",
+    punchline: "Milk and quackers." },
+  { setup: "How many tickles does it take to make an octupus laugh?",
+    punchline: "Ten Tickles" }
+];
+
+app.get('/joke', function (req, res) {
+  var randomIndex = Math.floor(Math.random() * jokes.length);
+  res.json(jokes[randomIndex]);
+});
