397f9f83bac376ffe5fc908f445ec736ed3a000a
Taylor Richmond <taylorichmond@gmail.com>
2015-06-10 16:28:22 -0700

    added comments to server.js

--- server.js
+++ server.js
@@ -22,16 +22,20 @@ app.get("/fact", function (req, res) {
   res.send(randomFacts[index]);
 });
 
+//array of json objects in jokes
 var jokes = [
-  { setup: "What's the difference between a guitar and a fish?",
-    punchline: "You can't tuna fish." },
-  { setup: "What do you get when you cross a cow and a duck?",
-    punchline: "Milk and quackers." },
-  { setup: "How many tickles does it take to make an octupus laugh?",
-    punchline: "Ten Tickles" }
+  { "setup": "What's the difference between a guitar and a fish?",
+    "punchline": "You can't tuna fish." },
+  { "setup": "What do you get when you cross a cow and a duck?",
+    "punchline": "Milk and quackers." },
+  { "setup": "How many tickles does it take to make an octupus laugh?",
+    "punchline": "Ten Tickles" }
 ];
 
+//create endpoint at ./joke url
 app.get('/joke', function (req, res) {
+  //creat a random number between 0 (inclusively) and the length of jokes (exclusively)
   var randomIndex = Math.floor(Math.random() * jokes.length);
+  //pull out json object at random index of jokes
   res.json(jokes[randomIndex]);
 });
